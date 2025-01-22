3a310067507d09acce581131ff977290a37e6b26
diducc <davideruocco2003@gmail.com>
2025-01-22 17:42:00 +0100

    header

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>HomePage - UniSA Gym</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+    <link rel="stylesheet" type="text/css" href="styleSheet/style.css">
 </head>
 <body>
     <?php include 'header.html'; ?>
--- profilo.php
+++ profilo.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="stylesheet/style.css">
+    <link rel="stylesheet" type="text/css" href="styleSheet/style.css">
     <title>Profilo</title>
 </head>
 <body>
