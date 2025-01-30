97e7ea0190721355e1a994865766543f075dddf5
Teddyno <xalex926@gmail.com>
2025-01-30 16:24:16 +0100

    Inizio Profilo

--- profilo.php
+++ profilo.php
@@ -8,6 +8,40 @@
 </head>
 <body>
     <?php include 'header.html'; ?>
+
+    <header class="indice">
+        <a href="#section1">Dati Personali</a>
+        <a href="#section2">abbonamento</a>
+        <a href="#section3">Sicurezza</a>
+        <a href="#section4">Log out</a>
+    </header>
+
+    <section id="section1">
+        <div class="dettagli-info">
+            <p class="titolo-info">Dati Personali</p>
+            <p>Contenuto della sezione 1.</p>
+        </div>
+    </section>
+    <section id="section2">
+        <div class="dettagli-info">
+            <p class="titolo-info">abbonamento</p>
+            <p>Contenuto della sezione 2.</p>
+        </div>
+    </section>
+    <section id="section3">
+        <div class="dettagli-info">
+            <p class="titolo-info">Sicurezza</p>
+            <p>Contenuto della sezione 3.</p>
+        </div>
+    </section>
+    <section id="section4">
+        <div class="dettagli-info">
+            <p class="titolo-info">Log out</p>
+            <p><button>Log out</button></p>
+        </div>
+    </section>
+    <script src="js/indiceDinamico.js"></script>
+
     <?php include 'footer.html'; ?>
 </body>
 </html>
