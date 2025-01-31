8fac116de13b17c1048a7a7c5cb61f9fbf2df42c
Carlo <c.longobardo5@studenti.unisa.it>
2025-01-31 12:06:22 +0100

    abbonamento

--- abbonamento.php
+++ abbonamento.php
@@ -8,6 +8,42 @@
 </head>
 <body>
     <?php include 'header.html'; ?>
+         <!-- Sezione abbonamenti -->
+         <h1>Abbonamenti</h1>
+        <div class="container">
+            <div>
+                <h2 style="background-color: #022873; color: white; padding: 20px; border-radius: 8px; text-align: center;">12 mesi</h2>
+                <p style="text-align: center;">Accesso completo per 1 anno</p>
+            </div>
+            <div>
+                <h2 style="background-color: #022873; color: white; padding: 20px; border-radius: 8px; text-align: center;">6 mesi</h2>
+                <p style="text-align: center;">Accesso completo per 6 mesi</p>
+            </div>
+            <div>
+                <h2 style="background-color: #022873; color: white; padding: 20px; border-radius: 8px; text-align: center;">1 giorno</h2>
+                <p style="text-align: center;">Pass giornaliero</p>
+            </div>
+        </div>
+
+        <!-- Sezione personal trainer -->
+        <h1>I Nostri Personal Trainer</h1>
+        <div class="container">
+            <div>
+                <h2 style="background-color: #022873; color: white; padding: 20px; border-radius: 8px; text-align: center;">Paolo V.I.T.</h2>
+                
+                <p style="text-align: center;"><strong>Telefono:</strong> Numero</p>
+                <p style="text-align: center;"><strong>Giorni:</strong> Disponibilità</p>
+                <p style="text-align: center;">Maschio</p>
+            </div>
+            <div>
+                <h2 style="background-color: #022873; color: white; padding: 20px; border-radius: 8px; text-align: center;">Tedro</h2>
+                
+                <p style="text-align: center;"><strong>Telefono:</strong> Numero</p>
+                <p style="text-align: center;"><strong>Giorni:</strong> Disponibilità</p>
+                <p style="text-align: center;">Maschio</p>
+            </div>
+        </div>
+    
     <?php include 'footer.html'; ?>
 </body>
 </html>
