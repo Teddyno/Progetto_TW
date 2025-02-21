8f5c60eb53754d22204171c4cc8e3eba2a398a7a
Teddyno <xalex926@gmail.com>
2025-02-21 19:25:47 +0100

    aggiunta username

--- login.php
+++ login.php
@@ -6,15 +6,92 @@
     <link rel="stylesheet" type="text/css" href="styleSheet/style.css">
     <link rel="stylesheet" type="text/css" href="styleSheet/styleLoginRegister.css">
     <title>UniSa Gym - Login</title>
+
+    <?php
+		function get_pwd($nickname_email,$db){
+				require 'db.php';
+
+                if(is_email($nickname_email)){
+                    $sql = "SELECT password FROM iscritti WHERE email=$1;";
+                } else {
+                    $sql = "SELECT password FROM iscritti WHERE nickname=$1;";
+                }
+
+				$prep = pg_prepare($db, "sqlPassword", $sql);
+				$ret = pg_execute($db, "sqlPassword", array($nickname_email));
+				if(!$ret) {
+					echo "ERRORE QUERY: " . pg_last_error($db);
+					return false;
+				}
+				else{
+					if ($row = pg_fetch_assoc($ret)){
+						$pass = $row['password'];
+						return $pass;
+					}
+					else{
+						return false;
+					}
+				}
+				pg_close($db);
+		}
+
+		function get_dati($nickname_email,$db){
+			require 'db.php';
+
+            if(is_email($nickname_email)){
+                $sql = "SELECT id, nome, nickname, email FROM iscritti WHERE email=$1;";
+            } else {
+                $sql = "SELECT id, nome, nickname, email FROM iscritti WHERE nickname=$1;";
+            }
+			$prep = pg_prepare($db, "sqlNome", $sql);
+			$ret = pg_execute($db, "sqlNome", array($nickname_email));
+			if(!$ret) {
+				echo "ERRORE QUERY: " . pg_last_error($db);
+				return false;
+			}
+			else{
+				if ($row = pg_fetch_assoc($ret)){
+                    $_SESSION['email']= $row['email'];
+                    $_SESSION['nickname']= $row['nickname'];
+					$_SESSION['idIscritto']= $row['id'];
+					$_SESSION['nome']= $row['nome'];
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+			pg_close($db);
+		}
+
+        function is_email($nickname_email){
+            if (filter_var($nickname_email, FILTER_VALIDATE_EMAIL)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+	?>
 </head>
 <body>
     <?php include 'header.php'; ?>
 
+    <?php
+        if(isset($_POST['nicknameEmail']))
+            $nickname_email = $_POST['nicknameEmail'];
+        else
+            $nickname_email = "";
+        if(isset($_POST['password']))
+            $pass = $_POST['password'];
+        else
+            $pass = "";
+    ?>
+
     <div class="contenitore-login">
         <h2 class="titolo-login">Accedi alla tua area riservata</h2>
-        <form class="form-login" action="login-manager.php" method="POST">
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Inserisci la tua email" required>
+        <form class="form-login" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            <label for="nicknameEmail">nickname/Email</label>
+            <input type="text" id="nicknameEmail" name="nicknameEmail" placeholder="Inserisci nickname o Email" required>
 
             <label for="password">Password</label>
             <input type="password" id="password" name="password" placeholder="Inserisci la tua password" required>
@@ -26,6 +103,40 @@
             </p>
         </form>
     </div>
+
+    <?php
+        require_once "db.php";
+
+        //CONNESSIONE AL DB
+        $db = pg_connect($connection_string) or die('Impossibile connettersi al database: ' . pg_last_error());
+
+
+        if(!empty($nickname_email) && !empty($pass)){
+        //if(!empty($pass)|| !empty($nickname_email)){
+            //chiama la funzione get_pwd che controlla
+            //se username esiste nel DB. Se esiste, restituisce la password (hash), altrimenti restituisce false.
+            $hash = get_pwd($nickname_email,$db);
+            if(!$hash){
+                echo "<p style=\"margin-top:100px;\"> L'utente con $nickname_email non esiste. <a href=\"login.php\">Riprova</a></p>";
+            }
+            else{
+                if(password_verify($pass, $hash)){
+                    //echo "<p>Login Eseguito con successo</p>";
+                    //Se il login è corretto, inizializziamo la sessione
+                    session_start();
+                    $_SESSION['autenticato']=true;
+                    get_dati($nickname_email,$db);
+    ?>
+                <script>
+                    window.location.href = "profilo.php";
+                </script>
+    <?php   
+                }else{
+                    echo '<p style=\"margin-top:100px;\">Username/Email o password errati. <a href="login.php">Riprova</a></p>';
+                }
+            }
+        }
+    ?>
     
     <?php include 'footer.html'; ?>
 </body>
--- registrati.php
+++ registrati.php
@@ -18,6 +18,10 @@
             $cognome = $_POST['cognome'];
         else
             $cognome = "";
+        if(isset($_POST['nickname']))
+            $nickname = $_POST['nickname'];
+        else
+            $nickname = "";
         if(isset($_POST['email']))
             $email = $_POST['email'];
         else
@@ -31,51 +35,19 @@
         else
             $repassword = "";
 
-        // Se il campo password è vuoto non effettuiamo nessun altro controllo
-        if (!empty($pass)){
-            // Se le due password sono diverse mostriamo un messaggio di errore
-            if($pass!=$repassword){
-                echo "<p style=\"margin-top:100px;\"> Hai sbagliato a digitare la password. Riprova</p>";
-                // a $pass e $repass assegniamo una stringa vuota in modo tale che nel modulo sticky non capariranno più le password errate
-                $pass = "";
-                $repassword = "";
-            }
-            else{
-                // Se la password è stata inserita e la password di conferma coincide, proseguiamo
-                //ANDREBBERO INSERITI ANCHE I CONTROLLI DEGLI ALTRI VALORI OBBLIGATORI
-                //....
-
-                require_once "db.php";
-
-                //CONNESSIONE AL DB
-                $db = pg_connect($connection_string) or die('Impossibile connettersi al database: ' . pg_last_error());
-	
-                //CONTROLLO SE L'UTENTE GIA' ESISTE
-                if(email_exist($email,$db)){
-                    echo "<p style=\"margin-top:100px;\"> Email $email già esistente. Riprova</p>";
-                }
-                else{
-                    //ORA posso inserire il nuovo utente nel db
-                    if(insert_utente($nome, $cognome, $email, $pass,$db)){
-                        echo "<p style=\"margin-top:100px;\"> Utente registrato con successo. Effettua il <a href=\"login.php\">login</a></p>";
-                    }
-                    else{
-                        echo "<p style=\"margin-top:100px;\"> Errore durante la registrazione. Riprova</p>";
-                    }
-                }
-            }
-        }
-
     ?>
 
     <div class="contenitore-login">
         <h2 class="titolo-login">Crea il tuo account</h2>
         <form class="form-login" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
             <label for="nome">Nome</label>
-            <input type="text" id="nome" name="nome" placeholder="Inserisci il tuo nome" required>
+            <input type="text" id="nome" name="nome" placeholder="Inserisci il tuo nome" value="<?php echo $nome ?>" required>
 
             <label for="nome">Cognome</label>
-            <input type="text" id="cognome" name="cognome" placeholder="Inserisci il tuo cognome" required>
+            <input type="text" id="cognome" name="cognome" placeholder="Inserisci il tuo cognome" value="<?php echo $cognome ?>" required>
+
+            <label for="nome">nickname</label>
+            <input type="text" id="nickname" name="nickname" placeholder="Inserisci il tuo nickname" value="<?php echo $nickname ?>" required>
 
             <label for="email">Email</label>
             <input type="email" id="email" name="email" placeholder="Inserisci la tua email" required>
@@ -93,6 +65,49 @@
             </p>
         </form>
     </div>
+
+    <?php
+
+        
+        // Se il campo password è vuoto non effettuiamo nessun altro controllo
+        if (!empty($pass)){
+            // Se le due password sono diverse mostriamo un messaggio di errore
+            if($pass!=$repassword){
+                echo "<p style=\"margin-top:30px;\"> Hai sbagliato a digitare la password. Riprova</p>";
+                // a $pass e $repass assegniamo una stringa vuota in modo tale che nel modulo sticky non capariranno più le password errate
+                $pass = "";
+                $repassword = "";
+            }
+            else{
+                // Se la password è stata inserita e la password di conferma coincide, proseguiamo
+                //ANDREBBERO INSERITI ANCHE I CONTROLLI DEGLI ALTRI VALORI OBBLIGATORI
+                //....
+
+                require_once "db.php";
+
+                //CONNESSIONE AL DB
+                $db = pg_connect($connection_string) or die('Impossibile connettersi al database: ' . pg_last_error());
+	
+                //CONTROLLO SE L'UTENTE GIA' ESISTE
+                if(email_exist($email,$db)){
+                    echo "<p style=\"margin-top:30px;\"> Email $email già esistente. Riprova</p>";
+                    exit;
+                }
+                if(nickname_exist($nickname,$db)){
+                    echo "<p style=\"margin-top:30px;\"> Nickname $nickname già esistente. Riprova</p>";
+                }else{
+                    //ORA posso inserire il nuovo utente nel db
+                    if(insert_utente($nome, $cognome, $nickname, $email, $pass,$db)){
+                        echo "<p style=\"margin-top:30px;\"> Utente registrato con successo. Effettua il <a href=\"login.php\" style=\"text-decoration:none;\">login</a></p>";
+                    }
+                    else{
+                        echo "<p style=\"margin-top:30px;\"> Errore durante la registrazione. Riprova</p>";
+                    }
+                }
+            }
+        }
+
+    ?>
     
     <?php include 'footer.html'; ?>
 </body>
@@ -115,7 +130,7 @@ function email_exist($email,$db){
 	else{
 		// $row sarà uguale a false se non sono state restituite righe della tabella
 		// a seguito dell'esecizone del prepared statement.
-		// Nelle specifico, è false se la tabella non contiene un record con username uguale a $user
+		// Nelle specifico, è false se la tabella non contiene un record con nickname uguale a $user
 		if ($row = pg_fetch_assoc($ret)){
 			return true;
 		}
@@ -126,12 +141,33 @@ function email_exist($email,$db){
 	pg_close($db);
 }
 
-function insert_utente($nome,$cognome, $email, $pass,$db){
+function nickname_exist($nickname,$db){
+
+	$sql = "SELECT nickname FROM iscritti WHERE nickname=$1";
+	$prep = pg_prepare($db, "sqlnickname", $sql);
+	$ret = pg_execute($db, "sqlnickname", array($nickname));
+
+	if(!$ret) {
+		echo "ERRORE QUERY: " . pg_last_error($db);
+		return false;
+	}
+	else{
+		if ($row = pg_fetch_assoc($ret)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	pg_close($db);
+}
+
+function insert_utente($nome,$cognome, $nickname, $email, $pass,$db){
 
 	$hash = password_hash($pass, PASSWORD_DEFAULT);
-	$sql = "INSERT INTO iscritti(nome, cognome, email, password) VALUES($1, $2, $3, $4)";
+	$sql = "INSERT INTO iscritti(nome, cognome, nickname, email, password) VALUES($1, $2, $3, $4, $5)";
 	$prep = pg_prepare($db, "insertUser", $sql);
-	$ret = pg_execute($db, "insertUser", array($nome, $cognome, $email, $hash));
+	$ret = pg_execute($db, "insertUser", array($nome, $cognome, $nickname, $email, $hash));
 	if(!$ret) {
 		echo "ERRORE QUERY: " . pg_last_error($db);
 		return false;
